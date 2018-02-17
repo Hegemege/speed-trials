9849ce3380ba2af453addefc1fcdff921dbcc32a
Henri Niva <henri.niva@aalto.fi>
2018-02-17 04:20:33 +0200

    Update publish script

--- publish_prod.sh
+++ publish_prod.sh
@@ -3,6 +3,7 @@ cd server
 npm run build-prod
 cd ..
 cp -R server/dist/. /var/www/speed-trials
+rm -rf /var/www/speed-trials/static
 cp -R dist/. /var/www/speed-trials/static
 pm2 restart speedtrials
 
